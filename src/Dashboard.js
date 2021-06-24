1d1629ff784cba512997a975d7a6529de9061d22
NikolayValev <nikolaivalev@gmail.com>
2021-06-24 13:15:48 -0400

    Added the Dashboard, Watchlist, Grid and styling.

---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+const useStyles = makeStyles(styles);
+export default function Dashboard(params) {
+  const classes = useStyles();
+}
---
+++ src/Watchlist.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default class Watchlist extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={
+      count:0,
+    };
+  }
+
+  componentDidMount(){
+
+  }
+
+  componentDidUpdate(){
+
+  }
+  componentWillUnmount(){
+
+  }
+  
+
+}
