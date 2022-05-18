dbcddae604d1dc4dbd18551410a81705f91113f0
Nikolay Valev <45887392+NikolayValev@users.noreply.github.com>
2022-05-18 18:23:18 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -3,33 +3,55 @@ import logo from './stock_logo.svg';
 import Dashboard from "./views/Dashboard";
 import Layout from 'Layout';
 import './App.css';
-import { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {
+  Component
+} from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Card from "./components/Card/Card"
-const{Provider,Consumer} = React.createContext();
+const {
+  Provider,
+  Consumer
+} = React.createContext();
 
 export default class App extends Component {
-  state ={
+  state = {
     user: {
       name: 'Ivan Ivanov',
       number: '2018872287'
     }
   }
-  render(){
-    return (
-      <Provider value={this.state}>
-        <div className="App">
-          <p>Fuck you.</p>
-          <BrowserRouter>
-    <Switch>
-      <Route path="/admin" component={Layout} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>
-          {/*<Dashboard/>*/}
-        </div>
-      </Provider>
+  render() {
+    return ( <
+      Provider value = {
+        this.state
+      } >
+      <
+      div className = "App" >
+      <
+      p > Fuck you. < /p> <
+      BrowserRouter >
+      <
+      Switch >
+      <
+      Route path = "/admin"
+      component = {
+        Layout
+      }
+      /> <
+      Redirect from = "/"
+      to = "/admin/dashboard" / >
+      <
+      /Switch> < /
+      BrowserRouter > {
+        /*<Dashboard/>*/
+      } <
+      /div> < /
+      Provider >
     );
   }
 }
-
